b003b2da4de74657d762a9abc75112cdbd47722a
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2022-12-09 09:24:07 -0300

    Add files via upload

---
+++ Js/index.js
@@ -0,0 +1,20 @@
+let menuBtn = document.querySelector("#btn-mobile");
+let closeBtn = document.querySelector("#close-menu");
+
+function toggleMenu(event) {
+    if (event.type === "touchstart") event.preventDefault();
+    const nav = document.querySelector("#nav");
+    nav.classList.toggle("active");
+    const active = nav.classList.contains("active");
+    event.currentTarget.setAttribute("aria-expanded", active);
+}
+
+menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("touchstart", toggleMenu);
+
+function closeMenu() {
+    const nav = document.querySelector("#nav");
+    nav.classList.toggle("active");
+}
+
+closeBtn.addEventListener("click", closeMenu);
